6646a17feed5c8f0e549888fb8af67344b52718d
nick <nick@legenex.com>
2026-01-15 10:16:25 +0000

    File changes

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -48,7 +48,7 @@ export default function History() {
   
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#F5F5F7] via-white to-[#E1E1E5]">
-      <div className="max-w-lg mx-auto px-6 py-8">
+      <div className="max-w-4xl mx-auto px-6 py-8">
         {/* Header */}
         <motion.div
           initial={{ opacity: 0, y: -20 }}
